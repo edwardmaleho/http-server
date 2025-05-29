589b3e32eb49ce96dac403a5e47294a581ef9cd4
Edward Maleho <Edward.Maleho@dsa.de>
2025-05-29 17:45:29 +0200

    rename Connection to Session because scope increased

--- src/main.cpp
+++ src/main.cpp
@@ -3,21 +3,21 @@
 #include "HttpRequest.hpp"
 #include <iostream>
 #include "RequestHandler.hpp"
-#include "Connection.hpp"
+#include "Session.hpp"
 
 void async_accept_loop(boost::asio::io_context& io_context, boost::asio::ip::tcp::acceptor& acceptor) {
     boost::asio::ip::tcp::socket socket(io_context);
     auto handler = std::make_shared<RequestHandler>("MyServer/1.0", "www");
-    auto connection = std::make_shared<Connection>(std::move(socket));
+    auto session = std::make_shared<Session>(std::move(socket));
 
-    acceptor.async_accept(connection->get_socket(), [connection, &acceptor, &io_context, handler](const boost::system::error_code& ec) {
+    acceptor.async_accept(session->get_socket(), [session, &acceptor, &io_context, handler](const boost::system::error_code& ec) {
         if (!ec) {
-            connection->session_loop();
+            session->session_loop();
         };
         async_accept_loop(io_context, acceptor);
     });
 }
-
+ 
 int main() {
     int port = 8081;
     boost::asio::io_context io_context;
